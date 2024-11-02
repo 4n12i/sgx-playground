9838741d38b9e4d8880fffd4d3d6609b20c1f0bc
anrin <an12inakamu12a@gmail.com>
2024-11-02 18:23:17 +0900

    refactor

--- App/App.cpp
+++ App/App.cpp
@@ -14,9 +14,22 @@
 #define ENCLAVE_NAME "enclave.signed.so"
 #define SEALED_DATA_FILE "sealed.dat"
 
+sgx_enclave_id_t global_eid = 0;
+
 void ocall_print_status(sgx_status_t ret)
 {
     std::cout << "sgx_status_t: " << ret << std::endl;
+}
+
+/* OCALL (Outside CALL) 
+ * enclave -> app
+ * temporarily move from inside the enclave to outside the enclave (EEXIT)
+ * and call a function outside the enclave
+ * */
+void ocall_example() 
+{
+    std::cout << "output from OCALL" << std::endl;
+    return;
 }
 
 static bool write_buf_to_file(const char *filename, uint8_t *sealed, uint32_t sealed_len)
@@ -41,15 +54,47 @@ static bool write_buf_to_file(const char *filename, uint8_t *sealed, uint32_t se
     return true;
 }
 
-/* OCALL (Outside CALL) 
- * enclave -> app
- * temporarily move from inside the enclave to outside the enclave (EEXIT)
- * and call a function outside the enclave
- * */
-void ocall_example() 
+bool test_seal_data()
 {
-    std::cout << "output from OCALL" << std::endl;
-    return;
+    sgx_status_t ret = SGX_ERROR_UNEXPECTED;
+    uint8_t *sealed;
+    uint32_t sealed_len = 0;
+
+    ret = ecall_calc_sealed_len(global_eid, &sealed_len);
+    sealed = new uint8_t[sealed_len];
+
+    /* encrypt the data in enclave */
+    ret = ecall_seal_data(global_eid, sealed_len, sealed);
+    if (ret != SGX_SUCCESS)
+    {
+        std::cerr << "failed to seal the secret" << std::endl;
+        return false;
+    }
+
+    /* save the sealed blob */
+    if (!write_buf_to_file(SEALED_DATA_FILE, sealed, sealed_len)) 
+    {
+        std::cerr << "failed to save the sealed data blob to \"" << SEALED_DATA_FILE << "\"" << std::endl;
+        return false;
+    }
+
+    std::cout << "sealing data succeeded" << std::endl;
+    return true;
+}
+
+bool test_example()
+{
+    sgx_status_t ret = SGX_ERROR_UNEXPECTED;
+    int retval = -9999;
+    ret = ecall_example(global_eid, &retval);
+    if (ret != SGX_SUCCESS) 
+    {
+        std::cerr << "failed to execute ECALL" << std::endl;
+        return false;
+    }
+
+    std::cout << "execute ECALL" << std::endl;
+    return true;
 }
 
 static sgx_status_t initialize_enclave(const char* enclave_path, sgx_enclave_id_t *eid) 
@@ -67,8 +112,7 @@ static sgx_status_t initialize_enclave(const char* enclave_path, sgx_enclave_id_
 int main()
 {
     /* initialize the enclave */
-    sgx_enclave_id_t eid = 0;
-    sgx_status_t ret = initialize_enclave(ENCLAVE_NAME, &eid);
+    sgx_status_t ret = initialize_enclave(ENCLAVE_NAME, &global_eid);
     if (ret != SGX_SUCCESS)
     {
         std::cerr << "failed to run the enclave." << std::endl;
@@ -76,39 +120,16 @@ int main()
     }
 
     /* start ECALL */
-    int retval = -9999;
-    std::cout << "execute ECALL" << std::endl;
-    sgx_status_t status = ecall_example(eid, &retval);
-    if (status != SGX_SUCCESS) 
+    if (!test_example()) 
     {
-        std::cerr << "failed to execute ECALL" << std::endl;
-        return -1;
+        std::cerr << "test_example failed" << std::endl;
     }
-
-    uint8_t *sealed;
-    uint32_t sealed_len = 0;
-
-    ret = ecall_calc_sealed_len(eid, &sealed_len);
-    sealed = new uint8_t[sealed_len];
-
-    ret = ecall_seal_data(eid, sealed_len, sealed);
-    if (ret != SGX_SUCCESS)
+    if (!test_seal_data())
     {
-        std::cerr << "failed to seal the secret and save it to a file" << std::endl;
-        return -1;
+        std::cerr << "test_seal_data failed" << std::endl;
     }
-
-    /* save the sealed blob */
-    if (!write_buf_to_file(SEALED_DATA_FILE, sealed, sealed_len)) 
-    {
-        std::cerr << "failed to save the sealed data blob to \"" << SEALED_DATA_FILE << "\"" << std::endl;
-        return -1;
-    }
-
-    std::cout << "sealing data succeeded" << std::endl;
-
     /* destroy theh enclave */
-    sgx_destroy_enclave(eid);
+    sgx_destroy_enclave(global_eid);
 
     std::cout << "successfully completed" << std::endl;
 
